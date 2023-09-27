b896f124270c0225ce80e3348bbfd47789ec06bb
Monisola <khairaah@gmail.com>
2023-09-27 18:04:56 +0000

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API call or replace with your actual API request
-    setTimeout(() => {
-      const apiResponse = { data: 'API response data' };
-      // Resolve the Promise with the API response data
-      resolve(apiResponse);
-      
-      // If there's an error, you can reject the Promise
-      // reject(new Error('API request failed'));
-    }, 1000); // Simulated delay of 1 second
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
