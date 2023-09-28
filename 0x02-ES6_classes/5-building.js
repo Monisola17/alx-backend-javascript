d89c372fd1dab030b8adb40a983625da921aad43
Monisola <khairaah@gmail.com>
2023-09-28 21:19:58 +0000

    Classes in ES6

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+class Building {
+  constructor(sqft) {
+    // Verify the type of 'sqft'
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    // Store the attribute with an underscore
+    this._sqft = sqft;
+  }
+
+  // Getter for the 'sqft' attribute
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method: subclasses must implement this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
